0733da2068153600925bc9317fb7c05cf68aea2e
Arthur Machado <arthurcomac32@gmail.com>
2023-01-27 13:33:25 -0300

    add dark theme

--- portifolio/src/components/NavBar.jsx
+++ portifolio/src/components/NavBar.jsx
@@ -1,16 +1,32 @@
-import { BsInstagram } from 'react-icons/bs'
-import { BsGithub } from 'react-icons/bs'
-import { BsLinkedin } from 'react-icons/bs'
+import { BsInstagram, BsGithub, BsLinkedin, BsFillSunFill, BsFillMoonStarsFill } from 'react-icons/bs'
+import { useTheme } from '../hooks/useTheme'
 
 export function NavBar() {
+
+  const { theme, setTheme } = useTheme()
 
   const instagram = `https://www.instagram.com/arthur.c.machado/`
   const github = `https://github.com/arthurcomac`
   const linkedin = `https://www.linkedin.com/in/arthur-machado-a72411219/`
   
-  return <div className="w-full h-24 text-[#7956e8] text-6xl flex items-center justify-center space-x-10">
-    <a href={instagram} target="blank"><BsInstagram /></a>
-    <a href={github} target="blank"><BsGithub /></a>
-    <a href={linkedin} target="blank"><BsLinkedin /></a>
-  </div>
+  return (
+    <div className="flex-col md:flex w-full h-24 text-[#7956e8] dark:text-slate-200 items-center justify-center md:relative">
+
+      <div className=" text-6xl flex space-x-10 items-center justify-center mt-4">
+        <a href={instagram} target="blank"><BsInstagram /></a>
+        <a href={github} target="blank"><BsGithub /></a>
+        <a href={linkedin} target="blank"><BsLinkedin /></a>
+      </div>
+
+      <div className='md:absolute md:ml-[75%] flex items-center justify-center mt-4'>
+
+        { 
+          theme === "ligth"
+            ? <BsFillMoonStarsFill onClick={() => setTheme("dark")} /> 
+              : <BsFillSunFill onClick={() => setTheme("ligth")} />
+        }
+
+      </div>
+    </div>
+  )
 }
--- portifolio/src/sections/About.jsx
+++ portifolio/src/sections/About.jsx
@@ -2,11 +2,11 @@ export function About () {
   
   return <div className="w-full m-0 md:flex">
     <img
-      src="../../public/images/imagemDeArthur.png"
+      src="../../images/imagemDeArthur.png"
       alt="imagem de arthur"
-      className="w-full md:w-1/2 rounded-3xl"
+      className="w-full md:w-1/2 md:h-auto rounded-3xl"
     />
-    <p className="md:w-1/3 md:ml-24 md:mt-24 font-roboto">
+    <p className="md:w-1/3 md:ml-24 md:mt-24 font-roboto dark:text-slate-100">
       <strong className="text-8xl">Olá</strong>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, 
       reiciendis soluta  maxime quisquam nisi sapiente eos molestias dolores corporis? 
@@ -15,7 +15,9 @@ export function About () {
       Lorem ipsum dolor, sit amet consectetur adipisicing 
       elit. Excepturi, tempora voluptatem. Vel omnis tenetur quia eaque officiis a, 
       fugit veritatis. Sapiente laborum aperiam perspiciatis quibusdam voluptatum error 
-      sequi? Aliquam, ex?
+      sequi? Aliquam, ex? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Natus aliquid eum veniam eos, quam voluptate. Voluptate, corporis id? Impedit est, 
+      error dolorum pariatur similique maiores ipsa ab mollitia. Numquam, cumque!
     </p>
   </div>
 }
