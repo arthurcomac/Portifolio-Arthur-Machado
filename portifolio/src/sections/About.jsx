bc1ad1ff1b584ed507e2ea82ccc4afa35ca6beec
Arthur Machado <arthurcomac32@gmail.com>
2023-10-26 23:43:21 -0300

    ajustado paragrafo

--- portifolio/src/sections/About.jsx
+++ portifolio/src/sections/About.jsx
@@ -7,11 +7,13 @@ export function About () {
         alt="imagem de arthur"
         className="w-full md:w-1/3 md:h-auto rounded-3xl"
       />
-      <p className="md:w-1/3 font-roboto dark:text-slate-100 text-lg mt-4 md:mt-10">
-        <strong className="text-8xl">Olá</strong>
-        meu nome é Arthur Machado.
-        Venha comigo dar um passeio pela minha jornada Dev!
-      </p>  
+      <div className="md:w-1/3 font-roboto dark:text-slate-100 text-lg mt-4 md:mt-10">
+        <strong className="text-5xl">Programador Frontend</strong>
+        <p>
+          Desenvolvendo possibilidades<br/>
+          Venha comigo dar um passeio pela minha jornada Dev!
+        </p>
+      </div>  
     </div>
   )
 }
